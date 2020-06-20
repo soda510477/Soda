fce5e46000e4eef3a0e1edc74b9484074493b3ca
soda510477 <ssooppppq@gmail.com>
2020-06-21 03:18:51 +0800

    Create Monster.java

---
+++ Monster.java
@@ -0,0 +1,10 @@
+
+public class Monster extends Creature {
+	public static int floor = 1;
+	public Monster() {
+		super();
+	}
+	public Monster(String n, int h,int a,int d) {
+		super(n, h, a, d);
+	}
+}
