440805128fffceff7b36e119b03ebe3b6a412afe
soda510477 <ssooppppq@gmail.com>
2020-06-21 03:19:00 +0800

    Create ShopGUI.java

---
+++ ShopGUI.java
@@ -0,0 +1,26 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.JComboBox;
+import javax.swing.BorderFactory;
+import javax.swing.JScrollPane;
+import javax.swing.JPanel;
+import java.awt.event.*;
+import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+
+public class ShopGUI extends JFrame {
+	private Container cp;
+	private JLabel nameLabel, hpLabel, atkLabel, defLabel, nm, hp, atk, def,layer;
+	private JButton shopButton, battleButton, exitButton, optionButton;
+	public Player player;
+	public ShopGUI(Player player){
+		
+	}
+}
